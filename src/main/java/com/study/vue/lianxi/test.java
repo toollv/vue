55a666c66663888225b35f481a57cd93dd592c3d
lv <1039779163@qq.com>
2021-10-22 19:47:43 +0800

    second commit

--- src/main/java/com/study/vue/lianxi/test.java
+++ src/main/java/com/study/vue/lianxi/test.java
@@ -1,6 +1,21 @@
 package com.study.vue.lianxi;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController("/1")
 public class test {
-    int v1;
+
+
+
+   @GetMapping("/1")
+    public String setV1(){
+
+        return "1";
+    }
+    @GetMapping("/2")
+    private String setV2(){
+
+        return "name";
+    }
 }
